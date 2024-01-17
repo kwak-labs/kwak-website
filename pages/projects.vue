3b3d2b4548fcc54af446b26e958fe8c875a84997
ducksquaddd <ducksquadddd@gmail.com>
2024-01-17 14:14:03 -0800

    add nomad stuff

---
+++ pages/projects.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main mt-7" style="text-align: center">
+    <h1 class="text-7xl font-bold title text-center text-white">Redirecting</h1>
+  </div>
+</template>
+
+<script setup>
+onMounted(() => {
+  window.top.location.href = "https://c7a0d2d8.kwak.pages.dev/projects";
+});
+</script>
