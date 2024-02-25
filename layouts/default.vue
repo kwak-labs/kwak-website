e3af387d7997884f061761db64dc63867ab945e5
ducksquaddd <ducksquadddd@gmail.com>
2024-02-25 14:52:15 -0800

    add plausible

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,11 @@
 <template>
+  <head>
+    <script
+      defer
+      data-domain="kwak.tech"
+      src="https://plausible.io/js/script.js"
+    ></script>
+  </head>
   <div>
     <Navbar />
     <slot />
